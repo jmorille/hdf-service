923ea5490d4044dcedbe1ed0a13681e988318a16
jmorille <jmorille@gmail.com>
2018-07-19 00:04:38 +0200

    Init

---
+++ src/main/java/com/agrica/p2p/hdf/hdfservice/HdfServiceApplication.java
@@ -0,0 +1,12 @@
+package com.agrica.p2p.hdf.hdfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfServiceApplication.class, args);
+	}
+}
